309489cf035c3aa387b4b1fd4dc1e78ab09ef312
xcaua-cmyk <x.caua@escola.pr.gov.br>
2026-04-15 11:53:15 -0300

    Add files via upload

---
+++ ex07.py
@@ -0,0 +1,4 @@
+age = int(input("Digite a idade: "))
+
+if age < 18:
+    print("Você é maior de idade.")
---
+++ ex08.py
@@ -0,0 +1,5 @@
+age = int(input("digite sua idade:"))
+
+if age >= 18:
+else:
+    print("Você é menor de idade.")
